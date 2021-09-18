d00de762a4fef03b72a02653d33d6c3ae68db2d3
indahputris <indahputrisnh@gmail.com>
2021-09-18 08:44:19 +0700

    penambahan isi contoh1.php

--- contoh1.php
+++ contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama Saya Indah Putri Suryaningsh
-        Saya tinggal di daerah Tanah Abang Jakarta Pusat
-        Saya menyukai matakuliah Web Programming";
+        echo "Nama Saya Indah Putri Suryaningsh";
+        echo "Saya tinggal di daerah Tanah Abang Jakarta Pusat"; 
+        echo "Saya Mahasiswa UBSI PEMUDA jurusan Sistem Informasi";
     }
 }
